c011e91f15a74136e7766f5cd0fb03df806d50fa
miller78130 <miller78130@gmail.com>
2021-08-15 13:31:35 -0500

    finally

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -9,9 +9,9 @@ var graymap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{
     accessToken: API_KEY
 });
 
-var map = L.map("map", {
-    center: [42,-42],
-    zoom: 3,
+var map = L.map("mapid", {
+    center: [0,0],
+    zoom: 2.4,
 });
 
 graymap.addTo(map);
@@ -20,12 +20,13 @@ var earthquakes = new L.LayerGroup();
 
 // Our AJAX call retrieves our earthquake geoJSON data.
 d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson", function(data) {
+    console.log(data);
 
 function styleInfo(feature) {
     return {
         opacity: 1,
         fillOpacity: 1,
-        fillColor: getColor(feature.properties.mag),
+        fillColor: getColor(feature.geometry.coordinates[2]),
         color: "#000000",
         radius: getRadius(feature.properties.mag),
         stroke: true,
@@ -33,23 +34,67 @@ function styleInfo(feature) {
     };
 }
 
-function getColor(magnitude) {
+function getColor(depth) {
     switch (true) {
-        case magnitude > 5:
+        case depth > 70:
             return "#f06b6b";
-        case magnitude > 4:
+        case depth > 50:
             return "#f0a76b";
-        case magnitude > 3:
+        case depth > 30:
             return "#f3ba4d";
-        case magnitude > 2:
+        case depth > 10:
             return "#f3db4d";
-        case magnitude > 1:
+        case depth > -10:
             return "#e1f34d";
         default:
             return "#b7f34d";
     }
 }
-function getRadius(magnitude)
-
+function getRadius(magnitude) {
+    if (magnitude === 0) {
+        return 1;
+    }
+    return magnitude *3;
 }
+L.geoJson(data, {
+    pointToLayer: function(feature, latlng) {
+        return L.circleMarker(latlng);
+    },
+    style: styleInfo,
+    onEachFeature: function(feature, layer) {
+        layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Depth: " + feature.geometry.coordinates[2] + "<br>Location: " + feature.properties.place);
+    }
+
+}).addTo(earthquakes);
+
+earthquakes.addTo(map);
+
+var legend = L.control({
+    position: "bottomright"
+});
+
+legend.onAdd = function() {
+    var div = L
+        .DomUtil
+        .create("div", "info legend");
+
+        var scale = [-10,10,30,50,70,90];
+        var colors = [
+            "#b7f34d",
+            "#e1f34d",
+            "#f3db4d",
+            "#f3ba4d",
+            "#f0a76b",
+            "#f06b6b"
+        ];
+
+        for (var i = 0; i< scale.length; i++) {
+            div.innerHTML += "<i style='background: " + colors[i] + "'></i>" +
+            scale[i] + (scale[i+1] ? "&ndash;" + scale[i+1] + "<br>" : "+");
+        }
+        return div;
+};
+legend.addTo(map);
+
+});
 
